1189dd345c3db848d1476b81746a3346a3be1c2f
bestelh <bestelh@oregonstate.edu>
2024-01-22 13:48:36 -0800

    Working picarx_week2 code complete with picarx_imp

--- picarx/picarx_week2.py
+++ picarx/picarx_week2.py
@@ -6,9 +6,9 @@ def main():
     px = Picarx()
     atexit.register(px.stopping_motors)
     while True:
-        command = input("Enter command: ") 
+        command = input("Enter command (F,B,3PT,PR,PL): ") 
         # input_command= input("Enter Command (action angle speed time): " )
-        # commands_to_array= input_command.split(" ")
+        # commands_to_array= input_command.split(" ")3pt
         
         if  command.upper() == 'F':
             px.set_dir_servo_angle(int(input("Enter angle: ")))
@@ -19,8 +19,50 @@ def main():
 
         elif command.upper() == 'B':
             px.set_dir_servo_angle(int(input("Enter angle: ")))
+            input_time= int(input("Run time (s): "))
             px.backward(int(input("Enter speed: ")))
-            time.sleep(int(input("Run time (s): ")))
+            time.sleep(input_time)
+            px.stop()
+
+        elif command.upper() == '3PT':
+            px.set_dir_servo_angle(10)
+            px.forward(90)
+            time.sleep(2)
+            px.set_dir_servo_angle(-25)
+            px.backward(90)
+
+            time.sleep(2)
+            px.set_dir_servo_angle(0)
+            px.forward(90)
+            time.sleep(1)
+            px.stop()
+
+        elif command.upper() == 'PR':
+            px.set_dir_servo_angle(30)
+            px.backward(80)
+            time.sleep(1)
+
+            px.set_dir_servo_angle(-30)
+            px.backward(80)
+            time.sleep(1.25)
+
+            px.set_dir_servo_angle(0)
+            px.forward(70)
+            time.sleep(0.35)
+            px.stop()
+
+        elif command.upper() == 'PL':
+            px.set_dir_servo_angle(-30)
+            px.backward(80)
+            time.sleep(1)
+
+            px.set_dir_servo_angle(30)
+            px.backward(80)
+            time.sleep(1.25)
+
+            px.set_dir_servo_angle(0)
+            px.forward(70)
+            time.sleep(0.35)
             px.stop()
 
         elif command.upper() == 'E':
