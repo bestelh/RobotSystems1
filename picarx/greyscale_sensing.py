b44ee4042c929deb5a589d23d32c3af39df899b6
bestelh <bestelh@oregonstate.edu>
2024-01-27 18:14:10 -0800

    grayscale testing

--- picarx/greyscale_sensing.py
+++ picarx/greyscale_sensing.py
@@ -1,4 +1,3 @@
-import atexit
 import time
 from picarx_improved import Picarx
 
@@ -9,16 +8,31 @@ except ImportError:
 
 class Sensing():
 
-    def __init__(self):
-        self.pin1, self.pin2, self.pin3 = ADC('A0'), ADC('A1'), ADC('A2')
+    def __init__(self, pin0, pin1, pin2):
+        
+        if isinstance(pin0,str):
+            self.chn_0 = ADC(pin0)
+            self.chn_1 = ADC(pin1)
+            self.chn_2 = ADC(pin2)
+        else:
+            self.chn_0 = ADC('A0')
+            self.chn_1 = ADC('A1')
+            self.chn_2 = ADC('A2')
 
-    def read_pins(self):
-        return self.pin1, self.pin2, self.pin3
+    def get_grayscale_data(self):
+        adc_value_list = []
+        adc_value_list.append(self.chn_0.read())
+        adc_value_list.append(self.chn_1.read())
+        adc_value_list.append(self.chn_2.read())
+        return adc_value_list
+    
+    def read(self):
+        return self.get_grayscale_data()
 
 if __name__ == "__main__":
     sensor = Sensing()
     while True:
-        print(sensor.read_pins())
+        print(sensor.read())
         time.sleep(0.1)  # Wait for 0.1 seconds
         command = input("Enter command: ")
         if command.upper() == 'EXIT':
